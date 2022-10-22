e233c2d91c408f5124fbe67fd5cf199f5206b01c
savinjamal <savinjamal@gmail.com>
2022-10-22 14:28:47 -0400

    first commit

---
+++ src/main/java/com/example/getmesocialservice/GetmesocialserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.getmesocialservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetmesocialserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetmesocialserviceApplication.class, args);
+	}
+
+}
