9115a059324a45cf5a387ac5891981d5f57889b3
savinjamal <savinjamal@gmail.com>
2022-11-03 20:43:06 -0400

    Seventh Commit Assignment 5

---
+++ src/main/java/com/example/getmesocialservice/resource/FileResource.java
@@ -0,0 +1,52 @@
+package com.example.getmesocialservice.resource;
+
+import com.amazonaws.services.s3.Headers;
+import com.amazonaws.services.s3.model.S3Object;
+import com.example.getmesocialservice.service.FileService;
+import org.apache.http.Header;
+import org.apache.http.protocol.HTTP;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/files")
+public class FileResource {
+
+    @Autowired
+    private FileService fileService;
+
+    @PostMapping
+    public boolean upload(@RequestParam (name = "file")MultipartFile file) {
+        return fileService.upload(file);
+    }
+
+    @GetMapping("/view")
+    public void view(@RequestParam(name="key") String key, HttpServletResponse httpServletResponse) throws IOException{
+
+        S3Object s3Object = fileService.getFile(key);
+        httpServletResponse.setContentType(s3Object.getObjectMetadata().getContentType());
+        httpServletResponse.getOutputStream().write(s3Object.getObjectContent().readAllBytes());
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<Resource> download(@RequestParam(name = "key") String key) throws IOException{
+        S3Object object = fileService.getFile(key);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(object.getObjectMetadata().getContentType()))
+                .header(Headers.CONTENT_DISPOSITION, "attachment; filename=\"" + key + "\"")
+                .body(new ByteArrayResource(object.getObjectContent().readAllBytes()));
+    }
+
+    @DeleteMapping
+    public void delete(@RequestParam(name="key") String key){
+        fileService.deleteFile(key);
+    }
+}
---
+++ src/main/java/com/example/getmesocialservice/service/FileService.java
@@ -0,0 +1,53 @@
+package com.example.getmesocialservice.service;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+public class FileService {
+
+    public boolean upload(MultipartFile file) {
+
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials("AKIAUQ5GS36I2ZNLIN4X", "DUWgDy3GtMqBcypqXF7JvTHboaU+d5SkFpYTjDL3");
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials)).withRegion(Regions.US_EAST_2).build();
+        try {
+
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentLength(file.getSize());
+            objectMetadata.setContentType(file.getContentType());
+
+            s3.putObject("backend-spring2020", file.getOriginalFilename(), file.getInputStream(), objectMetadata);
+            return true;
+        } catch (AmazonServiceException | IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public S3Object getFile(String key){
+
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials("AKIAUQ5GS36I2ZNLIN4X", "DUWgDy3GtMqBcypqXF7JvTHboaU+d5SkFpYTjDL3");
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials)).withRegion(Regions.US_EAST_2).build();
+        return s3.getObject("backend-spring2020", key);
+
+    }
+
+    public void deleteFile(String key){
+
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials("AKIAUQ5GS36I2ZNLIN4X", "DUWgDy3GtMqBcypqXF7JvTHboaU+d5SkFpYTjDL3");
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials)).withRegion(Regions.US_EAST_2).build();
+        s3.deleteObject("backend-spring2020", key);
+
+    }
+}
